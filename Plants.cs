c2fb7fd3c4f98707a60daf3f5983903523961937
Noah Allen <noahallen@Noahs-MacBook-Pro.local>
2025-01-07 21:14:33 -0500

    first commit

---
+++ Plants.cs
@@ -0,0 +1,14 @@
+namespace Plant{
+  
+  public class Plant{
+
+    public string Species {get; set;}
+    public  string LightNeeded {get; set;}
+    public int AskingPrice {get; set;}
+    public string City {get; set;}
+    public int ZIP {get; set;}
+    public bool Sold {get; set;}
+
+  }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Plant
+{
+  class Program
+  {
+
+    public static void Main()
+    {
+
+      List<Plant> plants = new List<Plant>
+      {
+        new Plant {Species = "Cactus", LightNeeded = "full sun", AskingPrice = 5, City = "Somerset", ZIP = 42503, Sold = false},
+        new Plant {Species = "Bamboo", LightNeeded = "some sun", AskingPrice = 7, City = "Richmond", ZIP = 42507, Sold = true},
+        new Plant {Species = "SugarCane", LightNeeded = "a lot sun", AskingPrice = 7, City = "Richmond", ZIP = 42507, Sold = false},
+        new Plant {Species = "Fern", LightNeeded = "little sun", AskingPrice = 10, City = "Florence", ZIP = 41005, Sold = true},
+        new Plant {Species = "DripLeaf", LightNeeded = "no sun", AskingPrice = 7, City = "Somerset", ZIP = 42503, Sold = false},
+      };
+
+      Console.WriteLine("Want Some Plants?");
+
+      foreach (var plant in plants)
+      {
+        Console.WriteLine($"Species: {plant.Species} | Light Needed: {plant.LightNeeded} | Asking Price: {plant.AskingPrice}, {plant.Sold} | Location: {plant.City},{plant.ZIP}");
+      }
+    }
+
+  }
+}
